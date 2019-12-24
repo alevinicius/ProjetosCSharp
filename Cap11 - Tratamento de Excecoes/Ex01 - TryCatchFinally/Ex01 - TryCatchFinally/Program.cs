742b4c6d3d775890c9cb6f39093a834f4aa936c8
alevinicius <alevinicius106@gmail.com>
2019-12-23 22:43:14 -0300

    Cat11 - Tratamento de Excecoes (Try Catch Finally)

---
+++ Cap11 - Tratamento de Excecoes/Ex01 - TryCatchFinally/Ex01 - TryCatchFinally/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex01___TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+                Console.WriteLine("Boa noite!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é permitido divisão por zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro de formação, digite um número inteiro");
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Até mais");
+            }
+        }
+    }
+}
