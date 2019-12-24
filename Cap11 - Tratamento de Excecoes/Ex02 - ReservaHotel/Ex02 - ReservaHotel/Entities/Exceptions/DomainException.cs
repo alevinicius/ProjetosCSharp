abea23da657a2fdb8ce9d97456ebe00abd8f0557
alevinicius <alevinicius106@gmail.com>
2019-12-24 14:32:35 -0300

    Ex02 - Reserva Hotel - Excecoes Personalizadas

---
+++ Cap11 - Tratamento de Excecoes/Ex02 - ReservaHotel/Ex02 - ReservaHotel/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex02.Entities.Exceptions
+{
+    class DomainException: ApplicationException
+    {
+        public DomainException(string message): base(message)
+        {
+
+        }
+    }
+}
---
+++ Cap11 - Tratamento de Excecoes/Ex02 - ReservaHotel/Ex02 - ReservaHotel/Entities/Reservation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Ex02.Entities.Exceptions;
+
+namespace Ex02.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get;  protected set; }
+        public DateTime CheckIn { get; protected set; }
+        public DateTime CheckOut { get; protected set; }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
