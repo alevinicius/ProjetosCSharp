f1e3feafbbcfd40f99f60edcd1fe0582d5805170
alevinicius <alevinicius106@gmail.com>
2019-12-26 21:59:34 -0300

    Criando a classe Posicao

---
+++ Cap12 - Xadrez/meu-xadrez/meu-xadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using tabuleiro;
+
+namespace meu_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3,4);
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
