bcb6f97525c2fa756ac0c6a820c9421bb5c47c20
alevinicius <alevinicius106@gmail.com>
2020-01-06 13:02:59 -0300

    Xadrez - Comecando a implementar o metodo que imprime o tabuleiro

--- Cap12 - Xadrez/meu-xadrez/Program.cs
+++ Cap12 - Xadrez/meu-xadrez/Program.cs
@@ -8,8 +8,9 @@ namespace meu_xadrez
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3,4);
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
