c3917ede99fd8345682f77f0fa105751a754a2a1
alevinicius <alevinicius106@gmail.com>
2020-01-06 14:48:11 -0300

    Criando a classe PosicaoXadrez(Converte posicao de xadrez pra matriz)

--- Cap12 - Xadrez/meu-xadrez/Program.cs
+++ Cap12 - Xadrez/meu-xadrez/Program.cs
@@ -9,20 +9,9 @@ namespace meu_xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
 
         }
     }
