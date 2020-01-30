42d3b26d68a524470120dd03635d6b41c1e75878
alevinicius <alevinicius106@gmail.com>
2020-01-29 22:50:43 -0300

    Topicos Especiais ExtensionMethods

---
+++ Cap15 - Tópicos Especiais/Ex01 ExtensionMethods/Ex01 ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DateTimeExtensions (method ElapsedTime)");
+            Console.WriteLine();
+            DateTime dt = new DateTime(2020, 01, 27, 21, 28, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("StringExtensions (method Cut)");
+            Console.WriteLine();
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+            Console.WriteLine(s1.Cut(25));
+            Console.WriteLine();
+        }
+    }
+}
