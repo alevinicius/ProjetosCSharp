27dd581e6760cf3e676b4aa61830aa685edd3fab
alevinicius <alevinicius106@gmail.com>
2020-02-04 23:04:14 -0300

    LINQ (Com varias expressoes Lambda)

---
+++ Cap16 - Lambda, Delegates e LINQ/Ex07Linq1/Ex07Linq01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex07Linq01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0) //Somente numeros pares
+                .Select(x => x * 10);   //Multiplica resultado por 10
+
+            //Execute de query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x); 
+                //Resultado vai ser 20 e 40
+            }
+        }
+    }
+}
