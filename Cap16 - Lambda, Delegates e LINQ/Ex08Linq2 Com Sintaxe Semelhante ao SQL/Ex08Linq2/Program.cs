f6eeaea673343c6f17cc704b7a5cf85d306094ea
alevinicius <alevinicius106@gmail.com>
2020-02-05 22:10:53 -0300

    LINQ Com Sintaxe Alternativa Semelhante ao SQL

---
+++ Cap16 - Lambda, Delegates e LINQ/Ex08Linq2 Com Sintaxe Semelhante ao SQL/Ex08Linq2/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Ex08.Entities;
+
+namespace Ex08
+{
+    class Program
+    {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach(T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Category c1 = new Category()
+            {Id = 1, Name = "Tools", Tier = 2};
+
+            Category c2 = new Category()
+            {Id = 2, Name = "Computers", Tier = 1};
+
+            Category c3 = new Category()
+            {Id = 3, Name = "Eletronics", Tier = 1};
+
+            List<Product> products = new List<Product>()
+            {
+                new Product(){
+                    Id = 1,
+                    Name = "Computer",
+                    Price = 1100.0,
+                    Category = c2
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Hammer",
+                    Price = 90.0,
+                    Category = c1
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Name = "TV",
+                    Price = 1700.0,
+                    Category = c3
+                },new Product()
+                {
+                    Id = 4,
+                    Name = "Notebook",
+                    Price = 1300.0,
+                    Category = c2
+                },
+                new Product()
+                {
+                    Id = 5,
+                    Name = "Saw",
+                    Price = 80.0,
+                    Category = c1
+                },
+                new Product()
+                {
+                    Id = 6,
+                    Name = "Tablet",
+                    Price = 700,
+                    Category = c2
+                },
+                new Product()
+                {
+                    Id = 7,
+                    Name = "Camera",
+                    Price = 700.0,
+                    Category = c3
+                },
+                new Product()
+                {
+                    Id = 8,
+                    Name = "Printer",
+                    Price = 350.0,
+                    Category = c3
+                },new Product()
+                {
+                    Id = 9,
+                    Name = "MacBook",
+                    Price = 1800.0,
+                    Category = c2
+                },
+                new Product()
+                {
+                    Id = 10,
+                    Name = "Sound Bar",
+                    Price = 700.0,
+                    Category = c3
+                },
+                new Product()
+                {
+                    Id = 11,
+                    Name = "Level",
+                    Price = 70.0,
+                    Category = c1
+                }
+            };
+
+            //Print foi uma função criada apenas para imprimir
+            
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00); 
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
+            Print("TIER 1 AND PRICE < 900", r1);
+
+            //var r2 = products.Where(p => p.Category.Id == 1).Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Id == 1
+                select p.Name;
+            Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new { p.Name, p.Price, CategoryName = p.Category.Name };
+            Print("PRODUCTS STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+
+            //Seleciona o que tem categoria de tier 1
+
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);            
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Price, p.Name
+                select p;
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+            //Pula os dois primeiros elementos de r4 e pega os 4 seguintes     
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME, SKIP 2, TAKE 4", r5);
+
+            //var r6 = products.FirstOrDefault();
+            var r6 = (from p in products select p).FirstOrDefault();
+            Console.WriteLine("First or default test1: " + r6);
+
+            //var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            var r7 = (from p in products where p.Price > 3000 select p).FirstOrDefault();
+            Console.WriteLine("First or default test2: " + r7);
+
+            Console.WriteLine();
+
+            //var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
+            var r8 = (from p in products where p.Id == 3 select p).SingleOrDefault();
+            Console.WriteLine("Single or default test 1: " + r8);
+
+            //var r9 = products.Where(p => p.Id == 33).SingleOrDefault();
+            var r9 = (from p in products where p.Id == 33 select p).SingleOrDefault();
+            Console.WriteLine("Single or default test 2: " + r9);
+            Console.WriteLine();
+
+            //DAQUI PRA BAIXO SÃO OPERAÇÕES DE AGREGAÇÃO
+            //NÃO APLIQUEI A SINTAXE ALTERNATIVA POR JÁ POSSUIR EXEMPLOS SUFICIENTES ACIMA
+            //EXCETO NO R16  LÁ EMBAIXO ONDE USEI O GROUPBY
+
+            var r10 = products.Max(p => p.Price);
+            Console.WriteLine("Max price: " + r10);
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine("Min price: " + r11);
+            var r12 = products
+                .Where(p => p.Category.Id == 1)
+                .Sum(p => p.Price);
+            Console.WriteLine("Category 1, Sum prices: " + r12);
+
+            //Média (mas se a busca for vazia dá exceção ao tentar calcular a média)
+            var r13 = products
+                .Where(p => p.Category.Id == 1)
+                .Average(p => p.Price);
+            Console.WriteLine("Category 1, Average prices: " + r13);
+
+            //Média (Sem dar exceção caso esteja vazio, pois retornará zero se vazio)
+            var r14 = products
+                .Where(p => p.Category.Id == 5)
+                .Select(p => p.Price)
+                .DefaultIfEmpty(0.0)
+                .Average();
+            Console.WriteLine("Category 5, Average prices: " + r14);
+
+            //OPERAÇÕES DE AGREGAÇÃO PERSONALIZADAS
+            
+            //Soma dos precos dos produtos da categoria 1
+            //0.0 é um valor inicial definido na agregação para proteger
+            //de exceção em caso de lista vazia
+            var r15 = products
+                .Where(p => p.Category.Id == 1)
+                .Select(p => p.Price)
+                .Aggregate(0.0, (x, y) => x + y); 
+            Console.WriteLine("Category 1, Aggregate sum: " + r15);
+            Console.WriteLine();
+
+            //Agrupar produtos por categoria e imprimir
+            
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products group p by p.Category;                
+            foreach (IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine("Category "+ group.Key.Name + ":");
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
